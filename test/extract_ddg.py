c46e9f832a6433f58b6267199e8754eb49dbe719
zmhoyer_mac <zmhoyer@gmail.com>
2024-11-13 11:56:25 -0500

    made some matlab and script changes

--- test/extract_ddg.py
+++ test/extract_ddg.py
@@ -1,12 +1,17 @@
 #! /usr/bin/env python
 
-import os, sys
+import os, sys, subprocess
 
-number_of_peptides = range(1,5)
-list_of_snapshots = [750000, 770000, 790000, 810000, 830000, 850000, 890000, 910000, 930000, 950000, 970000]
-list_of_residues = ['1THR']
+sim = range(1,5) 
+number = [750000, 770000, 790000, 810000, 830000, 850000, 890000, 910000, 930000, 950000, 970000]
+
+
+number_of_peptides = sim
+list_of_snapshots = number
+
 
 cwd = os.getcwd()
+list_of_residues = residueName(cwd+"/"+str(number_of_peptides[0])+"/"+str(list_of_snapshots[0]))
 for residue_file in list_of_residues:
     os.mkdir('ddg')
     os.chdir('ddg')
@@ -18,9 +23,12 @@ for residue_file in list_of_residues:
 for peptide in number_of_peptides:
     #I'm thinking maybe open a file here that's per
     for snapshot in list_of_snapshots:
+        peptide,snapshot = str(peptide),str(snapshot)
+        list_of_residues = residueName(cwd+"/"+peptide+"/"+snapshot)
         for residue in list_of_residues:
-            peptide,snapshot = str(peptide),str(snapshot)
             os.chdir(cwd+"/p"+peptide+"/"+snapshot+"ps/"+residue+"/extracted_elements")
+
+            subprocess.Popen("/programs/bin/matlab -nojvm")
 
             res = open(cwd+"/ddg/"+residue, 'w')
             #res.write() #gotta write the ddg to all of em
