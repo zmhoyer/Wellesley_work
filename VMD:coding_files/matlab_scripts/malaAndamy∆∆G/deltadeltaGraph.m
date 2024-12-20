0ecaa3af40818947cf181888fe91d0abd0cba68e
zmhoyer_mac <zmhoyer@gmail.com>
2024-12-20 11:19:08 -0500

    again, first time in a while

--- VMD:coding_files/matlab_scripts/malaAndamy∆∆G/deltadeltaGraph.m
+++ VMD:coding_files/matlab_scripts/malaAndamy∆∆G/deltadeltaGraph.m
@@ -1,23 +1,31 @@
+%general settings
 clear;
+close all
+blue = [57 106 177]./255;
+red = [204 37 41]./255;
+black = [83 81 84]./255;
+green = [62 150 81]./255;
+brown = [146 36 40]./255;
+purple = [107 76 154]./255;
 
+
+%script details
 peptides = 8;
-
 figure('Name','cumulative','NumberTitle','off');
-%
+%data and processing
 data_array1 = load("amys_residue_summary_all_peps.txt");
 data_array2 = load("residue_summary_all_peps.txt"); %from mala's sim
-
 combind_array = cat(1,data_array1,data_array2);
-
 data_mean = mean(combind_array,1);
 data_std = std(combind_array,1);
 data_ste = data_std/sqrt(peptides);
-%
+%figure settings
 myfontsize = 15;
-errorbar(data_mean, data_ste, '-r.', 'MarkerSize',20, 'LineWidth',2);
+errorbar(data_mean, data_ste, '-r.', 'MarkerSize',20, 'LineWidth',2, 'Color',blue);
 ylabel('\Delta\DeltaG_{opt,elec}, kcal/mol','fontsize',myfontsize);
 xlabel('BF2 residue','fontsize',myfontsize);
 ax1 = gca;
+
 %restricts the axises to be directly up on the data and then gives them
 %some centering space
 axis(ax1, 'tight');
