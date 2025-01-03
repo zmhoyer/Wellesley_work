9f02509c6a0bd7449fcbd8dfa39eaeb5d09ca1ad
zmhoyer_mac <zmhoyer@gmail.com>
2025-01-03 02:05:39 -0500

    been a bit, need to commit all my changes up there

---
+++ VMD:coding_files/matlab_scripts/3wt_analysis/xvg_to_text.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--files', help='files you want converted to .txt files without headings', required=True, nargs='+')
+parser.add_argument('-e', '--extension', help='what extension should these be changed to', required=False, default=".txt")
+args = parser.parse_args()
+
+file_list = args.files
+extension = args.extension
+
+for file in file_list:
+
+    file_name, original_extension = os.path.splitext(file)
+    read_file = open(file, 'r')
+    write_file = open(file_name+extension, 'w')
+
+    for line in read_file:
+        if line[0] == "@" or line[0] == "#":
+            pass
+        else:
+            write_file.write(line)
+
+    write_file.close()
+    read_file.close()
+
+
